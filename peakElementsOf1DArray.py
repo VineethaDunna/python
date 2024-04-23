83f83703b4e80cb6e556de76b10a38a0a6178ddf
VineethaDunna <159226063+VineethaDunna@users.noreply.github.com>
2024-04-23 19:01:43 +0530

    Update peakElementsOf1DArray.py

--- peakElementsOf1DArray.py
+++ peakElementsOf1DArray.py
@@ -1,3 +1,18 @@
+''' This question is asked in following companies
+Amazon
+Apple
+Bloomberg
+Facebook
+Google
+Microsoft
+Quora
+Uber'''
+
+# DESCRIPTION OF PROGRAM
+# Peak elements of an array are the elements that are greater than their adjacent elements.
+# The adjacent elements of an element at index are elements at index  i-1 and i+1.  For corner elements A[0] or A[N-1], there is onlyone adjacent element, i.e. either at index i+1 or i-1
+# Given an array A of N elements, write a program to print the indices of all peak elements in A in increasing order
+# SOLUTION
 n=int(input())
 list_1=list(map(int,input().split()))
 
