01a105754bce23d4be919ccdf9d245b48563577a
VineethaDunna <159226063+VineethaDunna@users.noreply.github.com>
2024-04-23 19:04:00 +0530

    Update NthFibonacciNumber.py

--- NthFibonacciNumber.py
+++ NthFibonacciNumber.py
@@ -22,3 +22,22 @@ def Fibonacci(n):
 
 n = int(input())
 print(Fibonacci(n))
+# Another solution with less time complexity.
+def fibonacci(n):
+    num1=1
+    num2=1
+    newlist=[]
+    for i in range(n):
+        if i==0:
+            
+            newlist.append(num1)
+        elif i==1:
+            
+            newlist.append(num2)
+        else:
+            value=newlist[i-1]+newlist[i-2]
+            newlist.append(value)
+    nth_value=newlist[n-1]
+    return nth_value
+n=int(input())
+print(fibonacci(n))
